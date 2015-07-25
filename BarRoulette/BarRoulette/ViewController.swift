4a39ca9212a3e350ac5045fd7746186e47fd2046
Emma Koszinowski <emkosz@gmail.com>
2015-07-24 18:13:16 -0700

    First commit

---
+++ BarRoulette/BarRoulette/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  BarRoulette
+//
+//  Created by Emma Koszinowski on 7/24/15.
+//  Copyright (c) 2015 Emma Koszinowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var yesButton: UIButton!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
