419dd4040aacab4dcb16e4c5616bc7f837cfd911
Emma Koszinowski <emkosz@gmail.com>
2015-08-13 14:49:09 +0200

    Added Yelp API and a search function that search for bars.

--- BarRoulette/BarRoulette/ViewController.swift
+++ BarRoulette/BarRoulette/ViewController.swift
@@ -12,9 +12,6 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var yesButton: UIButton!
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -25,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func didPressYesButton(sender: UIButton) {
+        println("you pressed the yes button")
+    }
 
 }
 
---
+++ BarRoulette/BarRoulette/search.swift
@@ -0,0 +1,45 @@
+//
+//  search.swift
+//  BarRoulette
+//
+//  Created by Emma Koszinowski on 8/3/15.
+//  Copyright (c) 2015 Emma Koszinowski. All rights reserved.
+//
+
+import Foundation
+
+let APIHost = "api.yelp.com"
+let searchPath = "/v2/search/"
+
+class APISample2: NSObject {
+    class func search(callback: ([String: AnyObject]?, NSError?) -> Void) {
+        let session = NSURLSession.sharedSession()
+        let params = [
+            "term": "bar",
+            "location": "New York, NY",
+            "category_filter": "divebars",
+        ]
+        session.dataTaskWithRequest(NSURLRequest(host: APIHost, path: searchPath, params: params)) { (data, response, error) -> Void in
+            if let data = data {
+                var decodingError: NSError?
+                if let decoded = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(), error: &decodingError) as? [String: AnyObject] {
+                    callback(decoded, nil)
+                } else {
+                    callback(nil, decodingError)
+                }
+            } else {
+                callback(nil, error)
+            }
+        }.resume()
+    }
+    
+    class func search2(callback: ([String: AnyObject]?, NSError?) -> Void) {
+        search { JSON, error in
+            if let JSON = JSON, firstBusiness = (JSON["businesses"] as? [AnyObject])?.first as? [String: AnyObject] {
+                firstBusiness["snippet_text"].map(println)
+            }
+            callback(JSON, error)
+        }
+    }
+}
+
