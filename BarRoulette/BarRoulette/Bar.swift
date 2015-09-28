6f13c71d21e7c6552eb5a607fc06bfe5894037dc
Emma Koszinowski <emkosz@gmail.com>
2015-09-27 22:03:50 -0700

    Added a Bar struct

--- BarRoulette/BarRoulette/Bar.swift
+++ BarRoulette/BarRoulette/Bar.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+struct Bar: CustomStringConvertible {
+    static func create(snippet snippet: String?, imageURL: NSURL?, coordinate: Coordinate?) -> Bar? {
+        if let x = snippet, y = imageURL, z = coordinate {
+            return Bar(snippet: x, imageURL: y, coordinate: z)
+        }
+        return nil
+    }
+    
+    let snippet: String
+    let imageURL: NSURL
+    
+    let coordinate: Coordinate
+    
+    var description: String {
+        return "{ snippet: \(snippet), imageURL: \(imageURL), coordinate: \(coordinate) }"
+    }
+}
+
+
+// Coordinate.create(latitude: "43", longitude: "-70")
+
+struct Coordinate: CustomStringConvertible {
+    static func create(latitude latitude: Double?, longitude: Double?) -> Coordinate? {
+        if let x = latitude, y = longitude {
+            return Coordinate(latitude: x, longitude: y)
+        }
+        return nil
+    }
+    
+    let latitude: Double
+    let longitude: Double
+    
+    
+    var description: String {
+        return "(\(latitude),\(longitude))"
+    }
+}
