e5761d9cf57144f37167d0fe17a5cf0432ce94dc
Emma Koszinowski <emkosz@gmail.com>
2015-10-06 21:24:09 -0700

    added a route file

---
+++ BarRoulette/BarRoulette/route.swift
@@ -0,0 +1,22 @@
+//
+//  route.swift
+//  BarRoulette
+//
+//  Created by Emma Koszinowski on 10/6/15.
+//  Copyright © 2015 Emma Koszinowski. All rights reserved.
+//
+
+import Foundation
+
+
+//count how many minutes it takes to walk from users current location to the bar
+
+// get the location of the bar
+
+// get the location of the user
+
+// get the walking rout from users location to bar
+
+// calculate how long it will take to walk 
+
+// if less then 15 min - give the user walking direction
